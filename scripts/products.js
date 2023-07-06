08b304d4bb8e57f42998db43f99323b2d42993ea
slowandrarecooked <airbendervineeth@gmail.com>
2023-07-06 16:47:11 +0530

    homepage 90% responsive,onto products page

--- scripts/products.js
+++ scripts/products.js
@@ -5,10 +5,13 @@ document.querySelector('#footer').innerHTML=footer()
 document.querySelector('.logo').addEventListener('click',()=>{
     location.href='./index.html'
 })
-
-const hamburger=document.querySelector('.hamburger')
-const navMenu=document.querySelector('.navlinks')
-hamburger.addEventListener('click',()=>{
-    hamburger.classList.toggle('active')
-    navMenu.classList.toggle('active')
-})
+let fetchData=async()=>{
+    try {
+        let result=await fetch('https://shy-ruby-chimpanzee-sari.cyclic.app/products')
+        let data=await result.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+fetchData()
